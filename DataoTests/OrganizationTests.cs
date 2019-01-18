6eaed4e9c5e0822c085481d7b8b8848bac5822d0
vebster123 <salam_max@mail.ru>
2019-01-18 17:56:31 +0500

    Add files via upload

---
+++ DataoTests/OrganizationTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Organization;
+using ExcelManager;
+using System.IO;
+
+namespace DataoTests
+{
+    [TestClass]
+    public class OrganizationTests
+    {
+        [TestMethod]
+        public void CreateHumanWithID()
+        {
+            // arrange
+            uint someRandomID = 123456789;
+
+            // act
+            Human Ivan = new Human(someRandomID);
+
+            // assert
+            uint actualID = Ivan.About.ID;
+            Assert.AreEqual(someRandomID, actualID, "Constructor has failed on Human's ID");
+        }
+
+        [TestMethod]
+        public void CreateClientWithID()
+        {
+            // arrange
+            uint someRandomID = 123456789;
+
+            // act
+            Client Ivan = new Client(someRandomID);
+
+            // assert
+            uint actualID = Ivan.About.ID;
+            Assert.AreEqual(someRandomID, actualID, "Constructor has failed on Human's ID");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TableFaultyPathTest()
+        {
+            // arrange
+            FileInfo path = new FileInfo(@"C:/random_folder/random.xlsx");
+
+            // act
+            Table.FillTable(path);
+
+            // assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void SaveEmptyTable()
+        {
+            // arrange
+
+            // act
+            Table.Update();
+
+            // assert is handled by ExpectedException
+        }
+
+    }
+}
